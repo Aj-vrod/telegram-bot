3389e32e6b92181b80c59f1d3ed12ea026fd61c1
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-13 14:01:15 +0200

    added first conditional to message action

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/music/, (msg) => {
+bot.onText(/\/mymusic/, (msg) => {
   const chatId = msg.chat.id;
   const url = "https://www.youtube.com/playlist?list=PLT7qNs_kSA8st0Jk1O7GVj5IHNSmnIdDm"
 
-  bot.sendMessage(chatId, url);
+  bot.sendMessage(chatId, `This is my master's playlist. Enjoy ${url}`);
 })
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, "Ready to serve")
+  if (msg.text.toString().toLowerCase().startsWith('hi')) {
+    bot.sendMessage(chatId, "Hello, master. I'm ready to serve")
+  }
+
 })
