47a1b64520fa3b0138fe33ea8e3c91e3bd5769ff
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-13 13:28:49 +0200

    wrote first response

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+})
 
