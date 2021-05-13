fb480a21f9e983d1d0405b0e20c7e2d8b43955a1
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-13 14:07:04 +0200

    added error handler

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+// Commands
+
 bot.onText(/\/mymusic/, (msg) => {
   const chatId = msg.chat.id;
   const url = "https://www.youtube.com/playlist?list=PLT7qNs_kSA8st0Jk1O7GVj5IHNSmnIdDm"
 
-  bot.sendMessage(chatId, `This is my master's playlist. Enjoy ${url}`);
+  bot.sendMessage(chatId, `This is my Creator's playlist. Enjoy ${url}`);
 })
+
+// On any message
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -18,4 +22,11 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, "Hello, master. I'm ready to serve")
   }
 
+})
+
+// Error handling
+
+bot.on('error', (error) => {
+  console.error(error)
+  return;
 })
